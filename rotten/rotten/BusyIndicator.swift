bee6775412ad090c83d10f7457f40f5ce92afc88
ramahadevan <ramahadevan@ebay.com>
2014-09-14 15:57:29 -0700

    added detail and list pages

---
+++ rotten/rotten/BusyIndicator.swift
@@ -0,0 +1,22 @@
+//
+//  BusyIndicator.swift
+//  rotten
+//
+//  Created by Mahadevan, Ramesh on 9/13/14.
+//  Copyright (c) 2014 GoliaMania. All rights reserved.
+//
+
+import UIKit
+
+class BusyIndicator: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
